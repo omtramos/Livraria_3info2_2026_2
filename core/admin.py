8d74df5f749e479359d89b48068b1c5600e99b6f
omtramos <omtramos2026@gmail.com>
2026-06-16 14:19:52 -0300

    feat: criação da entidade Compra integrada ao usuário do projeto

--- core/admin.py
+++ core/admin.py
@@ -7,7 +7,7 @@ from django.contrib.admin import ModelAdmin, register
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Editora, Livro, User
+from core.models import Autor, Categoria, Compra, Editora, Livro, User
 
 
 @register(Autor)
@@ -25,6 +25,13 @@ class CategoriaAdmin(admin.ModelAdmin):
     search_fields = ('descricao',)
     list_filter = ('descricao',)
     ordering = ('descricao',)
+    list_per_page = 10
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'status')
+    ordering = ('usuario', 'status')
     list_per_page = 10
 
 
