042127838c46a529a1af05ca8b877ede65105e9a
omtramos <omtramos2026@gmail.com>
2026-06-23 14:05:34 -0300

    feat: criação do endpoint para a listagem básica de compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,3 +3,4 @@ from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
 from .user import UserRegistrationSerializer, UserSerializer
+from .compra import CompraSerializer
